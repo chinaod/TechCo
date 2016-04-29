8b1cb025f545c7a017721d400f73369af4b32a9b
guo.tao.qin <guo.tao.qin@oracle.com>
2016-04-29 20:37:59 +0800

    移除maven中weblogic-server-pom依赖

--- src/main/java/com/oracle/cloud/demo/oe/sessions/OrderItemsFacade.java
+++ src/main/java/com/oracle/cloud/demo/oe/sessions/OrderItemsFacade.java
@@ -1,14 +1,12 @@
 package com.oracle.cloud.demo.oe.sessions;
 
 import com.oracle.cloud.demo.oe.entities.OrderItem;
-import weblogic.javaee.CallByReference;
 
 import javax.ejb.Stateless;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 import java.io.Serializable;
 
-@CallByReference
 @Stateless
 public class OrderItemsFacade extends AbstractFacade<OrderItem> implements Serializable, OrderItemsFacadeRemote {
     @PersistenceContext
